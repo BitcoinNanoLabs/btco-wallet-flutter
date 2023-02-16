fa2906275d10c703be357c077852c470e473ec83
u300638 <douglas.silva@hubfintech.com.br>
2023-02-16 14:49:41 -0300

    fix.

--- lib/ui/intro/intro_welcome.dart
+++ lib/ui/intro/intro_welcome.dart
@@ -6,8 +6,6 @@ import 'package:natrium_wallet_flutter/styles.dart';
 import 'package:natrium_wallet_flutter/localization.dart';
 import 'package:natrium_wallet_flutter/ui/widgets/buttons.dart';
 import 'package:flare_flutter/flare_actor.dart';
-import 'package:rive/rive.dart';
-import 'package:flutter/services.dart';
 
 class IntroWelcomePage extends StatefulWidget {
   @override
@@ -16,30 +14,6 @@ class IntroWelcomePage extends StatefulWidget {
 
 class _IntroWelcomePageState extends State<IntroWelcomePage> {
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
-  Artboard _riveArtboard;
-  RiveAnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    rootBundle.load('assets/welcome_animation.riv').then(
-      (data) async {
-        final file = RiveFile();
-
-        // Load the RiveFile from the binary data.
-        if (file.import(data)) {
-          // The artboard is the root of the animation and gets drawn in the
-          // Rive widget.
-          final artboard = file.mainArtboard;
-          // Add a controller to play back a known animation on the main/default
-          // artboard.We store a reference to it so we can toggle playback.
-          artboard.addController(_controller = SimpleAnimation('Animation 1'));
-          setState(() => _riveArtboard = artboard);
-          setState(() => _controller.isActive = true);
-        }
-      },
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -49,77 +23,81 @@ class _IntroWelcomePageState extends State<IntroWelcomePage> {
       backgroundColor: StateContainer.of(context).curTheme.backgroundDark,
       body: LayoutBuilder(
         builder: (context, constraints) => SafeArea(
-          minimum: EdgeInsets.only(
-            bottom: MediaQuery.of(context).size.height * 0.035,
-            top: MediaQuery.of(context).size.height * 0.10,
-          ),
-          child: Column(
-            children: <Widget>[
-              //A widget that holds welcome animation + paragraph
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    //Container for the animation
-                    Container(
-                      //Width/Height ratio for the animation is needed because BoxFit is not working as expected
-                      width: double.infinity,
-                      height: MediaQuery.of(context).size.width * 5 / 8,
-                      child: Center(
-                        child: _riveArtboard == null
-                            ? const SizedBox()
-                            : Rive(artboard: _riveArtboard),
-                      ),
-                    ),
-                    //Container for the paragraph
-                    Container(
-                      margin: EdgeInsets.symmetric(
-                          horizontal: smallScreen(context) ? 30 : 40,
-                          vertical: 20),
-                      child: AutoSizeText(
-                        AppLocalization.of(context).welcomeText,
-                        style: AppStyles.textStyleParagraph(context),
-                        maxLines: 4,
-                        stepGranularity: 0.5,
-                      ),
-                    ),
-                  ],
-                ),
+              minimum: EdgeInsets.only(
+                bottom: MediaQuery.of(context).size.height * 0.035,
+                top: MediaQuery.of(context).size.height * 0.10,
               ),
-
-              //A column with "New Wallet" and "Import Wallet" buttons
-              Column(
+              child: Column(
                 children: <Widget>[
-                  Row(
-                    children: <Widget>[
-                      // New Wallet Button
-                      AppButton.buildAppButton(
-                          context,
-                          AppButtonType.PRIMARY,
-                          AppLocalization.of(context).newWallet,
-                          Dimens.BUTTON_TOP_DIMENS, onPressed: () {
-                        Navigator.of(context)
-                            .pushNamed('/intro_password_on_launch');
-                      }),
-                    ],
+                  //A widget that holds welcome animation + paragraph
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        //Container for the animation
+                        Container(
+                          //Width/Height ratio for the animation is needed because BoxFit is not working as expected
+                          width: double.infinity,
+                          height: MediaQuery.of(context).size.width * 5 / 8,
+                          child: Center(
+                            child: FlareActor(
+                              "assets/welcome_animation.flr",
+                              animation: "main",
+                              fit: BoxFit.contain,
+                              color: StateContainer.of(context)
+                                  .curTheme
+                                  .primary,
+                            ),
+                          ),
+                        ),
+                        //Container for the paragraph
+                        Container(
+                          margin: EdgeInsets.symmetric(
+                              horizontal: smallScreen(context)?30:40, vertical: 20),
+                          child: AutoSizeText(
+                            AppLocalization.of(context).welcomeText,
+                            style: AppStyles.textStyleParagraph(context),
+                            maxLines: 4,
+                            stepGranularity: 0.5,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Row(
+
+                  //A column with "New Wallet" and "Import Wallet" buttons
+                  Column(
                     children: <Widget>[
-                      // Import Wallet Button
-                      AppButton.buildAppButton(
-                          context,
-                          AppButtonType.PRIMARY_OUTLINE,
-                          AppLocalization.of(context).importWallet,
-                          Dimens.BUTTON_BOTTOM_DIMENS, onPressed: () {
-                        Navigator.of(context).pushNamed('/intro_import');
-                      }),
+                      Row(
+                        children: <Widget>[
+                          // New Wallet Button
+                          AppButton.buildAppButton(
+                              context,
+                              AppButtonType.PRIMARY,
+                              AppLocalization.of(context).newWallet,
+                              Dimens.BUTTON_TOP_DIMENS, onPressed: () {
+                                Navigator.of(context)
+                                    .pushNamed('/intro_password_on_launch');
+                          }),
+                        ],
+                      ),
+                      Row(
+                        children: <Widget>[
+                          // Import Wallet Button
+                          AppButton.buildAppButton(
+                              context,
+                              AppButtonType.PRIMARY_OUTLINE,
+                              AppLocalization.of(context).importWallet,
+                              Dimens.BUTTON_BOTTOM_DIMENS, onPressed: () {
+                            Navigator.of(context).pushNamed('/intro_import');
+                          }),
+                        ],
+                      ),
                     ],
                   ),
                 ],
               ),
-            ],
-          ),
-        ),
+            ),
       ),
     );
   }
